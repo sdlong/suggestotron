8d46843649110062711143a1abd511ecc9a2a533
sdlong <sdlong@techbang.com.tw>
2015-03-21 01:03:57 +0800

    generate fuction upvote

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   root 'topics#index'
-  resources :topics
-
+  resources :topics do
+    member do
+      post 'upvote'
+    end
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
