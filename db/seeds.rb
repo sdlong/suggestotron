32f421e0c144c8b7640ed517551cab3380764bde
sdlong <sdlong@techbang.com.tw>
2015-03-21 01:36:26 +0800

    generate seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts 'Hello World!'
+puts "這個種子檔會自動創建 100 個 topics, 每個 topics 各 20 個 votes"
+puts "種子資料建立中"
+
+create_topics = for i in 1..100 do
+                  topic = Topic.new(title: "Topic no.#{i}", description: "這是用種子建立的第 #{i} 篇文章")
+                  topic.save
+                  for k in 1..20 do
+                    topic.votes.create
+                  end
+                end
+
+puts "種子資料建立完畢"
